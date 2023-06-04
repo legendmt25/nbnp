bb5fbac738879ea125d179259abf229c86edbfc9
Трајковски Мартин <martintrajkovski70@gmail.com>
2023-06-04 18:35:34 +0200

    1 to 2 steps done

---
+++ 2.orientdb_import.sh
@@ -0,0 +1,7 @@
+cd bin
+oetl.sh ../import/BreedLabels.json
+oetl.sh ../import/ColorLabels.json
+oetl.sh ../import/StateLabels.json
+oetl.sh ../import/SampleSubmission.json
+oetl.sh ../import/Test.json
+oetl.sh ../import/Train.json
